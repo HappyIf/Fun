0f67fb7a04e9d43a0f8e9a17bfb3d8cf63045af9
HappyIf <91533349+HappyIf@users.noreply.github.com>
2021-10-18 11:39:17 +0530

    Update url.php

--- url.php
+++ url.php
@@ -1,26 +1,15 @@
-<?php
-$value = "";
-if ( isset ( $_GET["value"] ) ) {
-  $value = $_GET["value"];
-}
-?>
-
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html lang="en">
 <head>
   <title>Web Protocol Handler Sample - Register</title>
   <script type="text/javascript">
     navigator.registerProtocolHandler("web+burger",
-                                  "https://happyif.github.io/Fun/url.html?uri=%s",
+                                  "http://www.google.co.uk/?uri=%s",
                                   "Burger handler");
   </script>
 </head>
 <body>
-  <h1>Web Protocol Handler Sample - Handler</h1>
-  <p>This web page is called when handling a <code>web+burger:</code> protocol action. The data sent:</p>
-  <textarea>
-<?php echo(htmlspecialchars($value, ENT_QUOTES, 'UTF-8')); ?>
-  </textarea>
+  <h1>Web Protocol Handler Sample</h1>
+  <p>This web page will install a web protocol handler for the <code>web+burger:</code> protocol.</p>
 </body>
 </html>
